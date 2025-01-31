f181539851df7e67c095966dc1d26dfaa7090c54
Larissa Alves Geraldo <51873691807@>
2025-01-31 11:26:25 -0300

    31/01/2024

--- back/api/urls.py
+++ back/api/urls.py
@@ -3,7 +3,6 @@ from .views import ProfessorDetail, listar_professores
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
-
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
